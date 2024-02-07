71d6df635ace16e7b2ac401dc139f81133375fe8
Chidiebere Nwigwe <chidonnwigz@gmail.com>
2024-02-06 23:47:44 -0700

    Put my images in an images folder

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let nextButton = document.getElementById("next");
 // onload, image should be displayed
 window.onload = function (){
 let img = document.createElement("img");
-img.setAttribute("src","bonusimage1.avif");
+img.setAttribute("src","./images/bonusimage1.avif");
 img.setAttribute("id","image");
 document.body.appendChild(img);
 }
@@ -12,33 +12,34 @@ document.body.appendChild(img);
 /// function for onclick of next button
 nextButton.addEventListener("click", () =>{
     let image = document.getElementById("image");
-    let imgarr = [ "bonusimage1.avif" , "bonusimage2.avif" , "bonusimage3.avif" ];
+    let imgarr = [ "./images/bonusimage1.avif" , "./images/bonusimage2.avif" , "./images/bonusimage3.avif" ];
     let value = image.src;
 
- if(value ==  "http://127.0.0.1:5500/bonusimage1.avif"){
-    image.src =  imgarr[1];
+ if(value ==  "http://127.0.0.1:5500/images/bonusimage1.avif"){
+    image.setAttribute("src",imgarr[1]);
+
  }
- if(value ==  "http://127.0.0.1:5500/bonusimage2.avif"){
-    image.src =  imgarr[2];
+ if(value ==  "http://127.0.0.1:5500/images/bonusimage2.avif"){
+    image.src = imgarr[2];
  }
- if(value ==  "http://127.0.0.1:5500/bonusimage3.avif"){
-    image.src =  imgarr[0];
+ if(value ==  "http://127.0.0.1:5500/images/bonusimage3.avif"){
+    image.src = imgarr[0];
  } 
 })
 
 /// function for onclick of previous button
 previousButton.addEventListener("click", () =>{
     let image = document.getElementById("image");
-    let imgarr = [ "bonusimage1.avif" , "bonusimage2.avif" , "bonusimage3.avif" ];
+    let imgarr = [ "./images/bonusimage1.avif" , "./images/bonusimage2.avif" , "./images/bonusimage3.avif" ];
     let value = image.src;
 
- if(value ==  "http://127.0.0.1:5500/bonusimage3.avif"){
+ if(value ==  "http://127.0.0.1:5500/images/bonusimage3.avif"){
     image.src =  imgarr[1];
  }
- if(value ==  "http://127.0.0.1:5500/bonusimage2.avif"){
-    image.src =  imgarr[0];
+ if(value ==  "http://127.0.0.1:5500/images/bonusimage2.avif"){
+    image.src = imgarr[0];
  }
- if(value ==  "http://127.0.0.1:5500/bonusimage1.avif"){
+ if(value ==  "http://127.0.0.1:5500/images/bonusimage1.avif"){
     image.src =  imgarr[2];
  }
 })
